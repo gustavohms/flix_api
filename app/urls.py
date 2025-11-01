9b5fac498ae63782fd8ad3baf11245e2a474d399
Gustavo Henrique <gustavohenrique.atenas@gmail.com>
2025-11-01 11:51:03 -0300

    feat(movies): Add CRUD endpoints for Movie

--- app/urls.py
+++ app/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from genres.views import GenreCreateListView, GenreRetrieveUpdateDestroyView
 from actors.views import ActorCreateListView, ActorRetrieveUpdateDestroyView
+from movies.views import MovieCreateListView, MovieRetrieveUpdateDestroyView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -9,5 +10,8 @@ urlpatterns = [
     path('genres/<int:pk>', GenreRetrieveUpdateDestroyView.as_view(),name='genre-detail-view'),
 
     path('actors/', ActorCreateListView.as_view(), name='actor-create-list'),
-    path('actors/<int:pk>/', ActorRetrieveUpdateDestroyView.as_view(), name='actor-retrieve-update-destroy')
+    path('actors/<int:pk>/', ActorRetrieveUpdateDestroyView.as_view(), name='actor-detail-view'),
+
+    path('movies/', MovieCreateListView.as_view(), name='movie-create-list'),
+    path('movies/<int:pk>/', MovieRetrieveUpdateDestroyView.as_view(), name='movie-detail-view')
 ]
