2fd8930f58a9b0fac73b9700fba455c3414d27ca
Gustavo Henrique <gustavohenrique.atenas@gmail.com>
2025-11-02 16:51:53 -0300

    retornando para código anterior

--- movies/serializers.py
+++ movies/serializers.py
@@ -3,19 +3,8 @@ from movies.models import Movie
 from genres.models import Genre
 from actors.models import Actor
 
-class MovieSerializer(serializers.Serializer):
-    title = serializers.CharField()
-    genre = serializers.PrimaryKeyRelatedField(
-        queryset=Genre.objects.all(),
-    )
-    release_date = serializers.DateField()
-    actors = serializers.PrimaryKeyRelatedField(
-        queryset=Actor.objects.all(),
-        many=True
-    )
-    resume = serializers.CharField()
 
-class MovieModelSerializer(serializers.ModelSerializer):
+class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
         fields = '__all__'
