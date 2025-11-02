e046785d260c6edd49cc0afa5415d45eb92b6856
Gustavo Henrique <gustavohenrique.atenas@gmail.com>
2025-11-02 17:15:42 -0300

    feat(movies): Enhance MovieModelSerializer with validation for release date and resume length

--- movies/serializers.py
+++ movies/serializers.py
@@ -4,7 +4,17 @@ from genres.models import Genre
 from actors.models import Actor
 
 
-class MovieSerializer(serializers.ModelSerializer):
+class MovieModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
         fields = '__all__'
+
+    def validate_release_date(self, value):
+        if value.year < 1990:
+            raise serializers.ValidationError('A data de lançaento não pode ser anterior a 1991.')
+        return value
+    
+    def validate_resume(self,value):
+        if len(value) > 200:
+            raise serializers.ValidationError('Resumo não deve ser maior do que 200 caracteres.')
+        return value
