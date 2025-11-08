719a654843cc5a9c5842815d204f9ac6c7b3737d
Gustavo Henrique <gustavohenrique.atenas@gmail.com>
2025-11-08 12:01:29 -0300

    feat(movies): Update MovieModelSerializer with enhanced validation and average rating calculation

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,3 +1,4 @@
+from django.db.models import Avg
 from rest_framework import serializers
 from movies.models import Movie
 from genres.models import Genre
@@ -5,16 +6,24 @@ from actors.models import Actor
 
 
 class MovieModelSerializer(serializers.ModelSerializer):
+    rate = serializers.SerializerMethodField(read_only=True)
     class Meta:
         model = Movie
         fields = '__all__'
+    
+    def get_rate(self, obj):
+        rate = obj.reviews.aggregate(Avg('stars'))['stars__avg']
 
+        if rate:
+            return round(rate,1)
+        return None
+       
     def validate_release_date(self, value):
-        if value.year < 1990:
-            raise serializers.ValidationError('A data de lançaento não pode ser anterior a 1991.')
+        if value.year < 1900:
+            raise serializers.ValidationError('A data de lançaento não pode ser anterior a 1900.')
         return value
     
     def validate_resume(self,value):
-        if len(value) > 200:
-            raise serializers.ValidationError('Resumo não deve ser maior do que 200 caracteres.')
+        if len(value) > 500:
+            raise serializers.ValidationError('Resumo não deve ser maior do que 500 caracteres.')
         return value
